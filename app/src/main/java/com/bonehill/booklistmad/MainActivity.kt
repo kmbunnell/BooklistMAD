78eaeca4714a00d8cd7f1838527609925b69945b
Kristina Bunnell <mrskbunnell@gmail.com>
2021-11-04 15:08:57 -0400

    clean up search ui

--- app/src/main/java/com/bonehill/booklistmad/MainActivity.kt
+++ app/src/main/java/com/bonehill/booklistmad/MainActivity.kt
@@ -3,6 +3,8 @@ package com.bonehill.booklistmad
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -12,6 +14,7 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -19,6 +22,7 @@ import com.bonehill.booklistmad.ui.Navigation.BottomNavItem
 import com.bonehill.booklistmad.ui.Navigation.BottomNavigationBar
 import com.bonehill.booklistmad.ui.Navigation.Navigation
 import com.bonehill.booklistmad.ui.book_list.BookListScreen
+import com.bonehill.booklistmad.ui.book_list.SearchBar
 import com.bonehill.booklistmad.ui.theme.BooklistMADTheme
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.reflect.Modifier
@@ -66,5 +70,5 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
 
-       // BookListScreen()
+
 }
